4c61d726b12659ce03f315770fd914dc4de842d8
BuildTools <luminai@gmail.com>
2018-05-06 14:53:28 -0400

    lr added config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+
+DROPBOX_LIB = "/Users/luisrueda/Dropbox/Library/"
+DROPBOX = "/Users/luisrueda/Dropbox/"
+DB_LOCATION = "/Users/luisrueda/Dropbox/scripts/book_db/book.txt" #FOR SAVING BOOKS READ
+LOCATION_PATH  = "/Users/luisrueda/Dropbox/scripts/book_db/locations.txt"
+LEARN_EXTENSIONS = ['.pdf', '.kindle', '.lnk', '.sof', '.yt']
+
+
